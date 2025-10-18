3a131f3e285881dc91001b09b8cc578472a21e85
Anderson Nascimento <anderltda@gmail.com>
2025-10-18 14:48:42 -0300

    ajusts

--- src/main/java/br/com/enginer/domain/rule/dto/Model.java
+++
@@ -1,8 +0,0 @@
-package br.com.enginer.domain.rule.dto;
-
-public interface Model<T> {
-	
-	public T getId();
-
-	public void setId(T id);
-}
--- src/main/java/br/com/enginer/infrastructure/adapter/outbound/comissao/entity/Comissao.java
+++
@@ -1,5 +0,0 @@
-package br.com.enginer.infrastructure.adapter.outbound.comissao.entity;
-
-public class Comissao {
-
-}
